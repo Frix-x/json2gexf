2a02c3ff848bbc1e8317b45563df1fad2b06845d
Frix-x <boisselier.felix@gmail.com>
2016-09-26 18:25:42 +0200

    Added a simple script to do the job

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # json2gexf
-JSON parser to GEXF optimised for JSON obtained with "Lost Circles" chrome extension
+JSON parser to CSV optimised for JSON obtained with "Lost Circles" chrome extension
+
+Usage : put your JSON in the same folder as convert.js and name it "fbScrap.json". Then type in a command window : "node convert.js"
--- convert.js
+++ convert.js
@@ -1,1 +1,8 @@
-// app file
+const fs = require("fs");
+
+var json = require("./fbScrap.json");
+
+fs.writeFileSync("nodes.csv", "id;label;dataUrl\n"
+    + json.nodes.map((el,i) => [i, el.name, el.dataUrl].join(";")).join("\n"));
+fs.writeFileSync("edges.csv", "source;target\n"
+    + json.links.map(el => [el.source, el.target].join(";")).join("\n"));
